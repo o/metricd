cd9ee7bca1b6273f3b96bef3e418bd660f939012
Osman Ungur <osmanungur@gmail.com>
2016-01-17 15:49:51 +0200

    Initial commit

---
+++ src/main/java/org/polimat/metricd/Application.java
@@ -0,0 +1,13 @@
+package org.polimat.metricd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hey!");
+    }
+}
